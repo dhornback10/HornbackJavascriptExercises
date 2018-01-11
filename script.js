58c863b29fee768d56b89fe2fc147c25feeb0612
David Hornback <Dave@Davids-MacBook-Pro-2.local>
2018-01-11 15:15:30 -0500

    Add Javascript files

---
+++ script.js
@@ -0,0 +1,73 @@
+
+// Exercise 1
+
+// var num = prompt("Enter a Number!", "Put your number here!")
+// var total = 0;
+// for (var x = 1;x <= num; x++){
+// 	total = total + x;
+// }
+// console.log("The total of your number is: " + total);
+
+//Exercise 2
+
+// var play = prompt("Do you want to play?");
+// var words = "";
+// do {if (play==="yes" || play === "Yes") {
+// 		var word = prompt("Enter a word");
+// 		var words = (words + " " + word); 
+// 		play = prompt("Do you want to play again?");
+// 	} else {
+// 		console.log("Well...shoot.");
+// 	};
+
+// }while(play==="yes" || play==="Yes");
+// console.log(words);
+
+
+//Exercise 3
+
+// var name = prompt ("What is your name?", "Type your name here!");
+// var answer = prompt("Would you like to print your name?");
+// var first = ("Hello. My name is " + name);
+// var again = "";
+// var exclaim = "";
+
+// if (answer==="yes" || answer==="Yes"){
+// 	console.log(first);
+// 	var again = prompt("Would you like to print that again?");
+// 	while (again === "yes" || again === "Yes"){
+// 		var exclaim = "!" + exclaim;
+// 		console.log("Hello. My name is " + name + exclaim);
+// 		var again = prompt("Would you like to print that again?");
+// 	};
+// } else {
+// 	console.log("Fine. Be that way.")
+// };
+
+
+//Exercise 4
+
+// var time = prompt("What time of day is it?", "Morning, Noon, or Evening");
+// var meal = "";
+// var food = "";
+// if (time === "morning" || time === "Morning"){
+// 	var meal = "breakfast.";
+// 	var food = "eggs and toast.";
+// 	console.log("Since it is " + time + ", you should be eating " + meal + " We suggest " + food);
+// } else if (time === "noon" || time === "Noon"){
+// 	var meal = "lunch.";
+// 	var food = "a salad.";
+// 	console.log("Since it is " + time + ", you should be eating " + meal + " We suggest " + food);
+// } else if (time === "evening" || time === "Evening"){
+// 	var meal = "dinner.";
+// 	var food = "chicken and rice.";
+// 	console.log("Since it is " + time + ", you should be eating " + meal + " We suggest " + food);
+// } else {
+// 	console.log("Why you gotta be difficult?");
+// }
+
+
+
+
+
+
